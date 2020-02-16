752ee121672be8e43b8198334d926c3d3be7f7ec
Addi Amaya <chris.amaya.caa@gmail.com>
2020-02-15 20:16:14 -0600

    first attempt at the function find_nb

--- Build_apile_of_cubes.py
+++ Build_apile_of_cubes.py
@@ -12,3 +12,14 @@ def find_nb(m):
         The integer n such as n^3 + (n-1)^3 + ... + 1^3 = m.
         Or -1 if there is no such n.
     """
+    # m is something we refer to
+    # find n in this function
+    # example: if m is 8 then n is 2
+    n = 1
+    if n ^ 3 == m: # base case
+        return n
+    else:
+        # TODO: figure out a way to add 1 to n each time (making it global maybe 
+        n += 1
+        find_nb(m)
+
