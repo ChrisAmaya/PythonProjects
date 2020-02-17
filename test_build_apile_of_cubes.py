1bfebaf40a872ad81728229cdff2c80e2ca69ad1
Addi Amaya <chris.amaya.caa@gmail.com>
2020-02-17 10:10:18 -0600

    Adjusted test_build_apile_of_cubes.py

--- test_build_apile_of_cubes.py
+++ test_build_apile_of_cubes.py
@@ -9,10 +9,10 @@ print("The expected result is 1, got", status)
 print(" ")
 # --------------------------------------------------------------------------------------------
 status = 0
-if B.find_nb(8) != 2:
+if B.find_nb(8) != -1:
     print("The Value of 8 as an argument is not returning the correct n value")
 status = B.find_nb(8)
-print("The expected result is, got", status)
+print("The expected result is -1, got", status)
 print(" ")
 # --------------------------------------------------------------------------------------------
 
