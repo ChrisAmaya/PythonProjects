897c2d9c8e024fed97c2ad3074acf2386e0b44fa
Addi Amaya <chris.amaya.caa@gmail.com>
2020-02-21 16:13:37 -0600

    Defined function in Vowel_Count.py

---
+++ Vowel_Count.py
@@ -0,0 +1,14 @@
+def getCount(str):
+    """
+    Purpose:
+        Determines how many vowels are inside a string
+    Pre-Condition:
+        None
+    Post-Condition:
+        None
+    :param str:
+         any string
+    :return:
+         a integer (count) of vowels in the given string
+    """
+    
