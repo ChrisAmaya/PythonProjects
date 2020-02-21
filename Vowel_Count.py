bd067737cbfd612694dfa42ef2de08809129907c
Addi Amaya <chris.amaya.caa@gmail.com>
2020-02-21 16:39:41 -0600

    Working getCount function in Vowel_Count with for loop

--- Vowel_Count.py
+++ Vowel_Count.py
@@ -11,4 +11,18 @@ def getCount(str):
     :return:
          a integer (count) of vowels in the given string
     """
-    
+    num_vowels = 0
+    for i in str:
+        if i == "a":
+            num_vowels += 1
+        elif i == "e":
+            num_vowels += 1
+        elif i == "i":
+            num_vowels += 1
+        elif i == "o":
+            num_vowels += 1
+        else:
+            num_vowels = num_vowels
+    return num_vowels
+
+
