8b25894f61b8a6e236edc593b8b6b63ae7e9f879
Addi Amaya <chris.amaya.caa@gmail.com>
2020-02-25 22:31:15 -0600

    Added a for loop for Convert_String_to_Camel_Case.py

--- Convert_String_to_Camel_Case.py
+++ Convert_String_to_Camel_Case.py
@@ -9,4 +9,17 @@ def to_camel_case(text):
     :return:
         String with Camel Case
     """
-    
+
+    Camel_version = ""
+
+    for i in range(1, len(text)): # First letter case
+        Camel_version += text[0]
+        if str(i) == "-" or "_": # dash/Underscore case
+            Camel_version += str(i+1).upper()
+        else:  # regular letter case
+            Camel_version += str(i).lower()
+    return Camel_version
+
+
+
+
