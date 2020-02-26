6b105c3aa2cc1e776fefd859e20ca810bdf0014a
Addi Amaya <chris.amaya.caa@gmail.com>
2020-02-25 22:17:22 -0600

    Inital Commit of Conver_String_to_Camel_Case.py with docstring

---
+++ Convert_String_to_Camel_Case.py
@@ -0,0 +1,12 @@
+def to_camel_case(text):
+    """
+    Purpose:
+        Converts a string to a Camel Case version of itself
+    Pre-Conditions:
+        instead of spaces, if "Text" Param has dash/underscore
+    Post-Conditions:
+        String
+    :return:
+        String with Camel Case
+    """
+    
