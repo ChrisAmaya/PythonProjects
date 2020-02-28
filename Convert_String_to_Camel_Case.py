655d9c4502bc3f73e1e724c6dedcde513d4d3fe2
Addi Amaya <chris.amaya.caa@gmail.com>
2020-02-28 17:55:58 -0600

    working Convert_string_to_Camel_Case.py with while loop

--- Convert_String_to_Camel_Case.py
+++ Convert_String_to_Camel_Case.py
@@ -3,7 +3,7 @@ def to_camel_case(text):
     Purpose:
         Converts a string to a Camel Case version of itself
     Pre-Conditions:
-        instead of spaces, if "Text" Param has dash/underscore
+        instead of spaces, if "Text" Param has more then one word, use dash/underscore
     Post-Conditions:
         String
     :return:
@@ -11,12 +11,15 @@ def to_camel_case(text):
     """
 
     Camel_version = ""
-    Camel_version = Camel_version + text[0]
-    for i in range(len(text)-1):  # First letter case
-        if str(i) == "-" or "_":  # dash/Underscore case
+    Camel_version += text[0]
+    i = 1
+    while i < (len(text)):
+        if text[i] == "_":
             Camel_version = Camel_version + str(text[i+1]).upper()
-        else:  # regular letter case
+            i += 2
+        else:
             Camel_version = Camel_version + str(text[i]).lower()
+            i += 1
     return Camel_version
 
 print(to_camel_case("please_work"))
