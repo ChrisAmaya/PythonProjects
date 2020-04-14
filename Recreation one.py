cded85e3c3d2e79fd078a7b75fdeaadb20ebb403
Addi Amaya <chris.amaya.caa@gmail.com>
2020-04-14 16:16:24 -0600

    inital commit with 2020 pycharm

---
+++ Recreation one.py
@@ -0,0 +1,10 @@
+def list_squared(m, n):
+    """
+    Purpose:
+        Find all integers between m and n whose sum of squared divisors is itself a square
+    Pre:
+        :param m: Positive integer larger then 1
+        :param n: Positive integer larger then m
+    :return:
+        Array of lists where each list is the divisor with its corresponding squared divisors
+    """
